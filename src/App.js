dbf8f06d5ec0e1db152fc6d88dae8eada2b07ac1
chenxq <358009028@qq.com>
2019-06-05 14:03:54 +0800

    book list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import BookList from './component/BookList';
+import { Provider } from 'react-redux';
+import store from './store';
+import { render } from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Provider store={store}>
+      <div className="bookList">
+         Library
+      </div>
+      <BookList/>
+  </Provider>
+);
 
 export default App;
